41fe1a92b5159359ebcd405b6f53f4320f318d4e
Zeuhz-Droid <davidadegbiran2020@gmail.com>
2023-03-14 13:07:55 +0100

    updated api, display data and server msgs

--- script.js
+++ script.js
@@ -1,19 +1,64 @@
+//CONNECT TO SOCKET
+
+// const socket = io();
+
 const main = document.querySelector(".main");
+const overlay = document.querySelector(".overlay");
+const inputBoard = document.querySelector(".input-board");
+const formOverlay = document.querySelector(".form-overlay");
+const btnSendOverlay = document.querySelector(".btn-send-overlay");
 
 const navMenu = document.querySelector(".nav-menu");
 const btnMenu = document.querySelector(".btn-menu");
 const btnMenuCancel = document.querySelector(".btn-cancel");
+const formChatbox = document.querySelector(".form-chatbox");
 
 // INPUT ELEMENTS FOR CLIENTS
 const chatInput = document.querySelector(".chatbox-input");
 const btnSend = document.querySelector(".btn-send");
 
-let inputEvent;
-
 btnMenu.addEventListener("click", showMenu);
 btnMenuCancel.addEventListener("click", hideMenu);
-chatInput.addEventListener("input", captureUserInput);
-btnSend.addEventListener("click", renderClientMessage);
+
+let data, username, present_id;
+//  DEALING WITH THE OVERLAY
+formOverlay.addEventListener("submit", async (e) => {
+  e.preventDefault();
+  username = e.target.elements["form-overlay-input"].value;
+
+  if (username && username.length > 1) {
+    overlay.classList.add("hide");
+    inputBoard.classList.add("hide");
+  }
+
+  data = await makeCallToAPI("", "", "POST", username);
+
+  renderServerMessage(data);
+});
+
+formChatbox.addEventListener("submit", async (e) => {
+  e.preventDefault();
+  // 1.) run input validation on client input
+  let clientMessage = e.target.elements["chatbox-input"].value;
+
+  if (!clientMessage || Number(clientMessage) > 99) {
+    return;
+  }
+
+  renderMessage(clientMessage);
+
+  e.target.elements["chatbox-input"].value = "";
+
+  if (present_id == 1) {
+    data = await makeCallToAPI(present_id, clientMessage);
+  }
+
+  if (clientMessage == 1) {
+    data = await makeCallToAPI(clientMessage);
+  }
+
+  renderServerMessage(data);
+});
 
 function showMenu() {
   navMenu.classList.remove("hide-menu");
@@ -26,25 +71,118 @@ function hideMenu() {
 }
 
 // DEALING WITH CLIENT INPUTS
-function captureUserInput(event) {
-  inputEvent = event;
-}
-
-function renderClientMessage(e) {
-  e.preventDefault();
-  let clientMessage = inputEvent.target.value;
-  // 1.) run some input validation on client input
-  if (!clientMessage || Number(clientMessage) > 99) {
-    return;
-  }
-
+function renderMessage(message) {
   // 2.) create markup for client text
   const markup = `
         <div class="msg client-message">
-          <p>${clientMessage}</p>
+          <p>${message}</p>
         </div>`;
-  //   4.) render text to DOM
 
+  //  3.) render text to DOM
   main.insertAdjacentHTML("afterbegin", markup);
-  inputEvent.target.value = "";
+}
+
+async function makeCallToAPI(id = "", endpoint = "", method = "GET", username) {
+  const options = {
+    method,
+    credentials: "include",
+    // withCredentials: true,
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+    },
+  };
+
+  const body = JSON.stringify({ username });
+
+  method == "POST" ? (options.body = body) : "";
+
+  console.log(options);
+
+  const res = await fetch(
+    `
+  http://localhost:3030/api/v1/chatbot/${id}/${endpoint}`,
+    options
+  );
+
+  return await res.json();
+}
+
+function renderServerMessage(data) {
+  const messageHTML = displayData(data);
+  const markup = `
+    <div class="msg server-message">
+      <p>${data.message}</p>
+      <br/>
+        ${messageHTML}
+    </div>
+  `;
+  main.insertAdjacentHTML("afterbegin", markup);
+}
+
+function displayData(data) {
+  const dataObj = data.data;
+
+  if (!dataObj) return "";
+
+  if (dataObj.instructions) {
+    const p = document.createElement("p");
+    dataObj.instructions.forEach((el) => {
+      p.insertAdjacentHTML(
+        "beforeend",
+        `${el.option}. ${el.instruction} <br/>`
+      );
+    });
+    return p.innerHTML;
+  }
+
+  if (dataObj.Items) {
+    const p = document.createElement("p");
+    dataObj.Items.forEach((el) => {
+      p.insertAdjacentHTML("beforeend", `${el.id}. ${el.item} <br/>`);
+    });
+    return p.innerHTML;
+  }
+
+  if (dataObj.orders) {
+    const div = document.createElement("div");
+    dataObj.orders.forEach((el) => {
+      const markup = `
+        <div> 
+          <span>${el.id}. ${el.dateCreated}</span>
+          <p>You Bought ${el.itemsCount} items</p>
+          <p>${el.items.forEach((elem) => {
+            `${elem.items},`;
+          })}</p>
+          <span>Total Amount: ${el.amount}</span>
+        </div>
+        <br/>
+      `;
+      div.insertAdjacentHTML("beforeend", markup);
+    });
+
+    return div.innerHTML;
+  }
+
+  // select item
+  if (dataObj.order) {
+    const div = document.createElement("div");
+    let count = 0;
+    dataObj.order.forEach((el) => {
+      const markup = `
+        <div> 
+          <span>${el.id}. ${el.dateCreated}</span>
+          <p>You Bought ${el.itemsCount} items</p>
+          <p>${el.items.forEach((elem) => {
+            `${count++}. ${elem.items} - $${elem.amount}\n`;
+          })}</p>
+          <span>Total Amount: ${el.amount}</span>
+        </div>
+        <br/>
+      `;
+      div.insertAdjacentHTML("beforeend", markup);
+    });
+
+    return div.innerHTML;
+  }
 }
